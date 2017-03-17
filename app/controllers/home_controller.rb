1ac65099e5c5f19ee6c03ab4c31a5609a633ef83
David Sanchez <sanchez.dav90@gmail.com>
2017-03-16 22:16:00 -0600

    App finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
   def index
-    array = []
-    @log_games = LogGame.eager_load(:user).all
+    @log_games = LogGame.select('id, user_id, sum(user_score) as total, count(user_id) as played').group(:user_id)
+    @log_games2 = LogGame.select('id, opponent_id, sum(opponent_score) as total, count(opponent_id) as played').group(:opponent_id)
+
+    @log_games.each do |log|
+      @log_games2.each do |log2|
+        if log.user_id == log2.opponent_id
+          log.total = log.total + log2.total
+          log.played = log.played + log2.played
+        end
+      end
+    end
+
   end
 
   def history
